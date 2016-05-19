4bfdabd25e1377de9376172465dab18a856f7978
John Williams <jodawi@gmail.com>
2016-05-18 20:44:12 -0700

    more .gitignore fix

---
+++ java/src/main/java/alyriant/datastructures/trie/SimpleTrieTest.java
@@ -0,0 +1,54 @@
+package alyriant.datastructures.trie;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Test.*;
+
+/**
+ * Created by johnwilliams on 5/18/16.
+ */
+public class SimpleTrieTest
+{
+
+    @Test
+    public void testNonNegativeIntToSignedByte() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testSignedByteToNonNegativeInt() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testAdd() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testContainsPrefix() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testContains() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testRemove() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void testPrintKeys() throws Exception
+    {
+
+    }
+}
