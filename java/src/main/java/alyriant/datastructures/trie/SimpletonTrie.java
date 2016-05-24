fdd9aa1d7c13d4af0724aad4f5020a93064811f3
John Williams <jodawi@gmail.com>
2016-05-23 22:37:13 -0700

    Added SimpletonTrie

---
+++ java/src/main/java/alyriant/datastructures/trie/SimpletonTrie.java
@@ -0,0 +1,109 @@
+package alyriant.datastructures.trie;
+
+/**
+ * A too-simple trie for Strings, supporting add, containsPrefix, contains, and remove.
+ * No Unicode support, only ASCII characters supported. No null checks.
+ *
+ * This implementation uses an ArrayList of Character for traversal, which is not terribly efficient.
+ */
+public class SimpletonTrie
+{
+    private final static int ARRAY_SIZE = 128;
+
+    private Node root;
+
+    /** Trie node is dumb data, with all logic in the outer class. */
+    private class Node
+    {
+        Node[] children;
+    }
+
+    /**
+     * Adds a string to the trie, if it doesn't already exist. Case-sensitive.
+     */
+    public void add(String s)
+    {
+        if (root == null)
+        {
+            root = new Node();
+            root.children = new Node[ARRAY_SIZE];
+        }
+
+        char characters[] = s.toCharArray();
+
+        Node current = root;
+
+        for (char c : characters)
+        {
+            if (current.children == null)
+            {
+                current.children = new Node[ARRAY_SIZE];
+            }
+            if (current.children[(int) c] == null)
+            {
+                current.children[(int) c] = new Node();
+            }
+            current = current.children[(int) c];
+        }
+
+        if (current.children == null)
+        {
+            current.children = new Node[ARRAY_SIZE];
+        }
+
+        if (current.children[0] == null)
+        {
+            current.children[0] = new Node(); // end of word marker
+        }
+    }
+
+    /**
+     * Returns true if the string is in the trie, either as an entire word or as a prefix of a word.
+     */
+    public boolean containsPrefix(String s)
+    {
+        return nodeAtEndOfPrefix(s) != null;
+    }
+
+    /**
+     * Returns true if the string is an entire word in the trie, and not just a prefix.
+     */
+    public boolean contains(String s)
+    {
+        Node n = nodeAtEndOfPrefix(s);
+        return n != null && n.children != null && n.children[0] != null;
+    }
+
+    /**
+     * Removes a string from the trie, if it exists.
+     */
+    public void remove(String s)
+    {
+        Node n = nodeAtEndOfPrefix(s);
+        if (n != null && n.children != null && n.children[0] != null)
+        {
+            n.children[0] = null;
+        }
+    }
+
+    /**
+     * Returns the trie node corresponding to the last character of the string, if the string is in the trie.
+     * Returns null if the string is not a prefix in the trie.
+     */
+    private Node nodeAtEndOfPrefix(String s)
+    {
+        Node   current   = root;
+
+        char characters[] = s.toCharArray();
+
+        for (char c : characters)
+        {
+            if (current.children == null || current.children[(int) c] == null)
+            {
+                return null;
+            }
+            current = current.children[(int) c];
+        }
+        return current;
+    }
+}
---
+++ java/src/main/java/alyriant/datastructures/trie/SimpletonTrieTest.java
@@ -0,0 +1,60 @@
+package alyriant.datastructures.trie;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class SimpletonTrieTest
+{
+    @Test
+    public void testContainsPrefix() throws Exception
+    {
+        SimpletonTrie s = new SimpletonTrie();
+        s.add("Office");
+        s.add("Officer");
+
+        assertTrue(s.containsPrefix("Off"));
+        assertTrue(s.containsPrefix("Office"));
+        assertTrue(s.containsPrefix("Officer"));
+
+        assertFalse(s.containsPrefix("Officers"));
+        assertFalse(s.containsPrefix("Offer"));
+    }
+
+    @Test
+    public void testContains() throws Exception
+    {
+        SimpletonTrie s = new SimpletonTrie();
+        s.add("Officer");
+
+        assertTrue(s.contains("Officer"));
+
+        assertFalse(s.contains("Office"));
+        assertFalse(s.contains("Officers"));
+    }
+
+    @Test
+    public void testRemove() throws Exception
+    {
+        SimpletonTrie s = new SimpletonTrie();
+        s.add("Office");
+        s.add("Officer");
+
+        assertTrue(s.contains("Office"));
+        assertTrue(s.contains("Officer"));
+
+        s.remove("Office");
+        assertFalse(s.contains("Office"));
+        assertTrue(s.contains("Officer"));
+
+        s.remove("Officer");
+        assertFalse(s.contains("Officer"));
+    }
+}
